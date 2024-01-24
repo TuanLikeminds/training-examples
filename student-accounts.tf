4c2dc33cf39807ba752446324a4fdc15d2315dca
TuanLikeminds <68030325+TuanLikeminds@users.noreply.github.com>
2024-01-24 16:44:28 +0530

    create iam accounts for students
    initial commit for testing

---
+++ student-accounts.tf
@@ -0,0 +1,9 @@
+variable "student_count" {
+  description = "Number of IAM student users to create"
+}
+
+resource "aws_iam_user" "iam_users" {
+  count = var.student_count
+
+  name = "likeminds-pingdevops-student-${count.index + 1}"
+}
